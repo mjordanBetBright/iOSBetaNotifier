00df2866b9e8d2516f0f95f3dd6773502b38a6e8
Mark Jordan <30830739+mjordanBetBright@users.noreply.github.com>
2018-02-15 16:53:32 +0000

    Add files via upload

---
+++ iOSBetaNotifier.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import praw
+import pdb
+# import re
+import os
+
+reddit = praw.Reddit('bot1')
+
+if not os.path.isfile("stickied_posts.txt"):
+    stickied_posts = []
+    # print("first run")
+else:
+    with open ("stickied_posts.txt", "r") as f:
+        stickied_posts = f.read()
+        stickied_posts = stickied_posts.split("\n")
+        stickied_posts = list(filter(None, stickied_posts))
+        # print(stickied_posts)
+	
+	
+subreddit = reddit.subreddit("iOSBeta")
+
+for submission in subreddit.hot(limit=5):
+    if submission.stickied and submission.id not in stickied_posts:
+        submission.save(submission.id)
+        stickied_posts.append(submission.id)
+        # print("Title: ", submission.title)
+        # print(stickied_posts)
+
+with open("stickied_posts.txt", "w") as f:
+    for post_id in stickied_posts:
+        f.write(post_id + "\n")
+        
